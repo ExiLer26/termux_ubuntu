9878fc12e7b9d20775f0737f064487762e76c05a
exiler25 <exiler29@yandex.com>
2025-05-27 17:52:28 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,5 +3,11 @@
 apt update &&
 apt upgrade -y &&
 apt install -y curl wget nano git &&
+apt-get install software-properties-common -y && 
+apt update && 
+add-apt-repository ppa:deadsnakes/ppa -y && 
+apt install -y python3.11 python3.11-full python3-pip && 
+wget https://bootstrap.pypa.io/get-pip.py && 
+python3 get-pip.py && python3 -m pip install --upgrade pip && 
 rm -rf update.sh && 
 clear
