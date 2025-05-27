5cc5c189b372e108810b66edb3be094bb13d034b
exiler25 <exiler29@yandex.com>
2025-05-27 17:49:33 +0300

    Update python3.11-install.sh

--- python3.11-install.sh
+++ python3.11-install.sh
@@ -1,6 +1,6 @@
 apt-get install software-properties-common -y && 
 apt update && 
 add-apt-repository ppa:deadsnakes/ppa -y && 
-apt install python3.11 python3.11-full python3-pip && 
+apt install -y python3.11 python3.11-full python3-pip && 
 wget https://bootstrap.pypa.io/get-pip.py && 
 python3 get-pip.py && python3 -m pip install --upgrade pip
