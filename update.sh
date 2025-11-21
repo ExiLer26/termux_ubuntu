bab01aef4bef1c3ce01d43dd9239ff865cbe3183
exiler25 <exiler29@yandex.com>
2025-11-21 12:54:17 +0300

    Update update.sh to download and move Minecraft client

--- update.sh
+++ update.sh
@@ -9,5 +9,9 @@ add-apt-repository ppa:deadsnakes/ppa -y &&
 apt install -y python3.11 python3.11-full python3-pip && 
 wget https://bootstrap.pypa.io/get-pip.py && 
 python3 get-pip.py && python3 -m pip install --upgrade pip && 
+mkdir mcbot && 
+wget https://github.com/MCCTeam/Minecraft-Console-Client/releases/download/20250522-285/MinecraftClient-20250522-285-linux-arm && 
+mv MinecraftClient-20250522-285-linux-arm mc && 
+mv mc mcbot/
 rm -rf update.sh && 
 clear
